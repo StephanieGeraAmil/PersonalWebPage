82edc5f7091156ee04320afc638c1a931ee4b296
Stephanie <stephanie.gera.amil@gmail.com>
2022-12-22 14:19:33 -0300

    adding router to redirect

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { HashRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    
   <React.StrictMode>
-      <Router>
-    <App />
-     </Router>
-  </React.StrictMode>
- ,
-  document.getElementById('root')
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
