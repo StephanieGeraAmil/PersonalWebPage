4c7055c39a4c373a6a981bb19792ff0e28ec9e52
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-25 13:08:41 -0300

    some cleanup to the code

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,61 +1,87 @@
-import React ,{useState,useEffect}from 'react'
-import { CurrentProject } from './currentProject';
-
-
-
+import React, { useState, useEffect } from "react";
+import { CurrentProject } from "./currentProject";
+let arrayOfProjects = [
+    {
+      id: 1,
+      name: "Jars",
+      photo: "/project-imgs/katt-yukawa-K0E6E0a0R3A-unsplash.jpg",
+      link: "",
+    },
+    {
+      id: 2,
+      name: "Truth",
+      photo: "/project-imgs/bible-unsplash.jpg",
+      link: "",
+    },
+    {
+      id: 3,
+      name: "Not Alone",
+      photo: "/project-imgs/hannah-busing-Zyx1bK9mqmA-unsplash.jpg",
+      link: "https://fight-loneliness.com/",
+    },
+  ];
 export const Projects = () => {
-    
-    const initialArrayOfProjects=[
-    {id:1, name: "Jars", photo:'/project-imgs/katt-yukawa-K0E6E0a0R3A-unsplash.jpg',link:""},
-    {id:2,name: "Truth", photo:'/project-imgs/bible-unsplash.jpg',link:""},
-    { id: 3, name: "Not Alone", photo: '/project-imgs/hannah-busing-Zyx1bK9mqmA-unsplash.jpg', link:"https://fight-loneliness.com/"}
-    ];
-    //https://elastic-kowalevski-f1c1a2.netlify.app/
-    const [arrayOfProjects,setArayOfProjects]=useState(initialArrayOfProjects);
-    
-    let timer;
-   
-    const slideRight=()=>{
-        clearInterval(timer);
-        const arr=arrayOfProjects;
-        arr.push( arr.shift() ) ;
-        setArayOfProjects(arr.slice());
-    }
-    const slideLeft=()=>{ 
-        clearInterval(timer);
-       const arr=arrayOfProjects;
-        arr.unshift( arr.pop() );
-        setArayOfProjects(arr.slice()); //the slice is in order to change the array because react wasn't seeing any difference so the re-render wasnt happening    
-    }
-    useEffect(()=>{
-       timer= setInterval(()=>{slideRight(); }, 5000);
-         
-    },[arrayOfProjects]);
-    
+  
+  //https://elastic-kowalevski-f1c1a2.netlify.app/
+  const [shouldMove, setShouldMove] = useState(false);
 
-    return (
-        <div className='Project-section'>
-            <div className='projectsTitle'>
-            <h1>Projects</h1>
-            </div>
-            <div className='showroom'>
-                    
-                       
-                        <button  data-aos="fade-right" data-aos-duration="500" className='prevbtn' onClick={()=>slideLeft()}>
-                             <img src={arrayOfProjects[0].photo} className='side left' alt ={arrayOfProjects[0].name}/>  
-                        </button>
-                    
-                        <CurrentProject proj={arrayOfProjects[1]}/>
-                        
-                        <button  data-aos="fade-left" data-aos-duration="500" className='nextbtn'onClick={()=>slideRight()}>
-                             <img src={arrayOfProjects[2].photo} className='side right' alt ={arrayOfProjects[2].name}/>  
-                        </button>
-                  
-                        
-                  
-                                  
-            </div>               
-        </div>
-    )
-}
+  let timer;
+
+  const slideRight = () => {
+    clearInterval(timer);
+    const elementToMove = arrayOfProjects.shift();
+    arrayOfProjects.push(elementToMove);
+    setShouldMove(!shouldMove);
+  };
+  const slideLeft = () => {
+    clearInterval(timer);
+    const elementToMove = arrayOfProjects.pop();
+    arrayOfProjects.unshift(elementToMove);
+    setShouldMove(!shouldMove);
+  };
+  useEffect(() => {
  
+    timer = setInterval(() => {
+      slideRight();
+    }, 5000);
+  }, [shouldMove]);
+
+  return (
+    <div className="Project-section">
+      <div className="projectsTitle">
+        <h1>Projects</h1>
+      </div>
+      <div className="showroom">
+        <button
+          data-aos="fade-right"
+          data-aos-duration="500"
+          className="prevbtn"
+          onClick={() => slideLeft()}
+        >
+          <img
+            src={arrayOfProjects[0].photo}
+            className="side"
+            title={arrayOfProjects[0].name}
+            alt={arrayOfProjects[0].name}
+          />
+        </button>
+
+        <CurrentProject proj={arrayOfProjects[1]} />
+
+        <button
+          data-aos="fade-left"
+          data-aos-duration="500"
+          className="nextbtn"
+          onClick={() => slideRight()}
+        >
+          <img
+            src={arrayOfProjects[2].photo}
+            className="side"
+            alt={arrayOfProjects[2].name}
+            title={arrayOfProjects[2].name}
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
