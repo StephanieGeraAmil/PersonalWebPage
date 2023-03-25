e896ee5437c3a6b4dd8524d473145458b3137794
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-25 11:37:59 -0300

    improing artist attribution

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React ,{useEffect}from 'react';
-import './styles/styles.css';
-import { Top } from './components/top';
-import { Stack } from './components/stack';
-import { Projects } from './components/projects';
-import { About } from './components/about';
-import { Links } from './components/links';
+import React, { useEffect } from "react";
+import "./styles/styles.css";
+import { Top } from "./components/top";
+import { Stack } from "./components/stack";
+import { Projects } from "./components/projects";
+import { About } from "./components/about";
+import { Links } from "./components/links";
 import Aos from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 
-const App=()=> {
- 
+const App = () => {
   useEffect(() => {
-   Aos.init({duration:500})
-  }, [])
-  return (  
-        <div className='App'>
-            <Top/>  
-         
-            <Stack/>
-            <Projects/>
-            <About/>
-            <Links/>
-        </div>
+    Aos.init({ duration: 500 });
+  }, []);
+  return (
+    <div className="App">
+      <Top />
+
+      <Stack />
+      <Projects />
+      <About />
+      <Links />
+     
+    </div>
   );
-}
+};
 
 export default App;
-
--- src/components/links.js
+++ src/components/links.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export const Links = () => {
-    return (
-         <div className="links-container">
-                {/* <a href="">
-                    <img src='/links/gmail.png'className="link" alt="gmail"/> 
-                </a> */}
-            
-                <a href="https://github.com/StephanieGeraAmil">
-                    <img src='/links/Github.png'className="link" alt="github"/> 
-                </a>  
-                <a href="https://www.linkedin.com/in/stephanie-gera-60b6b5a0/">
-                    <img src='links/Linkedin.png' className="link" alt="linkedin"/>  
-                </a> 
-                      
-            </div>
-    )
-}
+  return (<>
+    <div className="bottom">
+      <div className="links-container">
+        <a href="https://github.com/StephanieGeraAmil">
+          <img src="/links/Github.png" className="link" alt="github" />
+        </a>
+        <a href="https://www.linkedin.com/in/stephanie-gera-60b6b5a0/">
+          <img src="links/Linkedin.png" className="link" alt="linkedin" />
+        </a>
+        
+      </div>
+      <div className="attribution">
+        <a  href="https://icons8.com/illustrations/author/zD2oqC8lLBBA">
+          <p> Artist</p>
+        </a>
+      </div>
+    </div>
+    </>
+  );
+};
 // tambien usando reat router podria haber usado <Link to="https://github.com/Joeyryanbridges">
-
