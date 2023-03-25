adf0d6eb77c46830cf6dbabff3f37ff9f9a10262
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-24 23:40:48 -0300

    deleting unnecesary code

--- src/components/stack.js
+++ src/components/stack.js
@@ -1,47 +1,40 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 import Marquee from "react-fast-marquee";
 
-
 export const Stack = () => {
+  const initialArrayOfTech = [
+    { alt: "react", src: "/icons-lightTheme/REACT.png" },
+    { alt: "javascipt", src: "/icons-lightTheme/JS.png" },
+    { alt: "node", src: "/icons-lightTheme/NODE.png" },
+    { alt: "html5", src: "/icons-lightTheme/HTML5.png" },
+    { alt: "mongo", src: "/icons-lightTheme/MONGO.png" },
+    { alt: "css3", src: "/icons-lightTheme/CSS3.png" },
+    { alt: "redux", src: "/icons-lightTheme/REDUX.png" },
+    { alt: "postgres", src: "/icons-lightTheme/POSTGRE.png" },
+    { alt: "sass", src: "/icons-lightTheme/SASS.png" },
+    { alt: "figma", src: "/icons-lightTheme/FIGMA.png" },
+    { alt: "typescript", src: "/icons-lightTheme/TS.png" },
+    { alt: "lambda", src: "/icons-lightTheme/LAMBDA.png" },
+    { alt: "material-ui", src: "/icons-lightTheme/MUI.png" },
+  ];
 
-     const initialArrayOfTech=[
-        {alt: "react", src:'/icons-lightTheme/REACT.png'},
-        {alt: "javascipt", src:'/icons-lightTheme/JS.png'},
-        {alt: "node", src:'/icons-lightTheme/NODE.png'},
-        {alt: "html5", src: '/icons-lightTheme/HTML5.png'},   
-        {alt: "mongo", src:'/icons-lightTheme/MONGO.png'},
-        {alt: "css3", src: '/icons-lightTheme/CSS3.png'},
-         {alt: "redux", src: '/icons-lightTheme/REDUX.png'},
-        {alt: "postgres", src: '/icons-lightTheme/POSTGRE.png'},
-        {alt: "sass", src: '/icons-lightTheme/SASS.png' },
-        {alt: "figma", src: '/icons-lightTheme/FIGMA.png'},
-        {alt: "typescript", src: '/icons-lightTheme/TS.png'},
-          {alt: "lambda", src: '/icons-lightTheme/LAMBDA.png'},
-                    {alt: "material-ui", src: '/icons-lightTheme/MUI.png'}
-    ];
-   
-    useEffect(() => {
-
-        
-    }, [])
-   const handleClickOnTech=()=>{
-
-   }
-
-    return (
-        <div className='Stack-section'>
-            <div className="tech-container" >
-            <Marquee pauseOnHover={true }gradient={false} speed={65}>
-                {initialArrayOfTech.map((tech)=>(<img src={tech.src} className="tech" title={tech.alt} onClick={handleClickOnTech} key={tech.alt} />   ))}
-            </Marquee> 
-     
-
-            </div>
-            <div data-aos="fade-left" data-aos-duration="1000" className='stackTitle'>
-                <h2 >my Stack</h2>
-            </div>
-            
-            
-        </div>
-    )
-}
+  return (
+    <div className="Stack-section">
+      <div className="tech-container">
+        <Marquee pauseOnHover={true} gradient={false} speed={65}>
+          {initialArrayOfTech.map((tech) => (
+            <img
+              src={tech.src}
+              className="tech"
+              title={tech.alt}
+              key={tech.alt}
+            />
+          ))}
+        </Marquee>
+      </div>
+      <div data-aos="fade-left" data-aos-duration="1000" className="stackTitle">
+        <h2>my Stack</h2>
+      </div>
+    </div>
+  );
+};
